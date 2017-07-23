8210887283d182a161c6d65f1581b7c5cd4e6cc2
Oviraj <solaiman.diu@gmail.com>
2017-07-23 11:20:35 +0600

    Initial commit

---
+++ app/src/main/java/ingeniumbd/com/firebaseapp/User.java
@@ -0,0 +1,35 @@
+package ingeniumbd.com.firebaseapp;
+
+public class User {
+
+    private String name;
+    private String country;
+    private double weight;
+
+
+    public User(){}
+    public User(String n, String c, double w){
+        this.name=n;
+        this.country=c;
+        this.weight=w;
+
+    }
+    public String getName() {
+        return name;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public double getWeight() {
+        return weight;
+    }
+
+
+
+    @Override
+    public String toString() {
+        return name +" "+ country +" " +weight;
+    }
+}
